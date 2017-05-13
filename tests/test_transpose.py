227189761ee6d6573866623c87e7407448166829
Anton Smolin <smolin.anton@gmail.com>
2017-05-13 22:09:45 +0300

    test transpose

---
+++ tests/test_transpose.py
@@ -0,0 +1,15 @@
+from tempfile import TemporaryFile
+
+from PIL import Image
+import pytest
+
+from imgpy import Img
+
+
+@pytest.mark.parametrize('image', ('anima/bordered.gif', 'anima/clear.gif', 'fixed/bordered.jpg', 'fixed/clear.jpg'))
+def test_transpose(path, image):
+    with Img(fp=path(image)) as src, TemporaryFile() as tf:
+        src.transpose(Image.FLIP_LEFT_RIGHT)
+        src.save(fp=tf)
+        with Img(fp=tf) as dest:
+            assert (dest.width, dest.height, dest.n_frames) == (src.width, src.height, src.n_frames)
